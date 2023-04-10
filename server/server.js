f6db9028df173e0b578d8b477bf997f8708a384d
chatreeck7 <chatree.devl@gmail.com>
2023-04-10 19:54:02 +0700

    🏗️ Initial the server for preparing

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API running");
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
