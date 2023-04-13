3ff59204f08a2429a3b5dee11dc86bea8a0412a4
chatreeck7 <chatree.devl@gmail.com>
2023-04-13 23:34:34 +0700

    ➕ Add script to sendMessage and communicate with gpt-3.5-turbo model

--- src/utils/script.ts
+++ src/utils/script.ts
@@ -6,29 +6,52 @@ const openai = new OpenAIApi(
   })
 );
 
-export function sendMessage() {
-  openai
-    .createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        //   { role: "system", content: "You are a helpful assistant." },
-        {
-          role: "user",
-          content:
-          "Hello ChatGPT"
-            // "I need menus ! to reduce my time to think the receipt and ingredient per meal.",
-        },
-        //   {
-        //     role: "assistant",
-        //     content:
-        //       "The proeficient food assistant such as Gordon Ramsey, IAN Kittichai",
-        //   },
-      ],
-    })
-    .then((res) => {
-      console.log(res.data);
+export const sendMessage = async (ingredients: string[]) => {
+  let content = `I have ingrediecns such as: `;
+  let result = "";
+  // formatted list of ingredients into the natural language format
+  const formattedIngredients = (ingredients: string[]) => {
+    ingredients.map((ingredient) => {
+      content += ` ${ingredient}, `;
     });
-}
+  };
+
+  formattedIngredients(ingredients);
+
+  content += `Can you cook 3 meals (prefer Thai food) with these ingredients for me ? And please return in this format: \nMenu: ..... \nCooking direction: \n \t 1: .... \n \t 2: .... .`;
+
+  console.log("content: ", content);
+
+  try {
+    const response = await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          {
+            role: "user",
+            content: content,
+          },
+          {
+            role: "assistant",
+            content:
+              "The proeficient food assistant such as Gordon Ramsey, IAN Kittichai",
+          },
+        ],
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.choices[0].message?.content) {
+          result = res.data.choices[0].message?.content;
+        }
+        return res.data;
+      });
+    console.log("Response: ", response);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export function getGptApiKey() {
   return process.env.REACT_APP_GPT_API_KEY;
