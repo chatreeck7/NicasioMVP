1909042af91ce9b9b6d73cd233254a7c3901de27
chatreeck7 <chatree.devl@gmail.com>
2023-04-22 10:53:36 +0700

    📥 Bootstrap the package, database, server for authentication service

--- server/server.js
+++ server/server.js
@@ -1,19 +1,43 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+var cors = require("cors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//CORS
+app.use(cors());
+//Body parser
+app.use(express.json());
 
+//Mout routers
+const auth = require("./routes/auth");
+
+//Cookie parser
+app.use(cookieParser());
 app.get("/", (req, res) => {
-  res.send("API running");
   res.status(200).json({ success: true, data: { id: 1 } });
 });
 
-app.listen(
+app.use("/api/v1/auth", auth);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
